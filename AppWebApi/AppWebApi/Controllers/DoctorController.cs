8592c62146383f4da3b0aabd18101effedda8c63
Uriel Quiroz <uriel.quiroz187@gmail.com>
2020-11-19 10:28:26 -0600

    Conceptos basicos Web API
    Se creo el metodo para obtener una lista de doctores

---
+++ AppWebApi/AppWebApi/Controllers/DoctorController.cs
@@ -0,0 +1,41 @@
+﻿using AppWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AppWebApi.Controllers
+{
+    public class DoctorController : ApiController
+    {
+        MedicoEntities db;
+
+        public DoctorController()
+        {
+            db = new MedicoEntities();
+        }
+
+        // GET: api/Doctor
+        [System.Web.Http.HttpGet]
+        public IEnumerable<DoctorModel> ListarDoctor()
+        {
+            IEnumerable<DoctorModel> listDoctor = (from d in db.Doctor
+                                                   join c in db.Clinica on d.IIDCLINICA equals c.IIDCLINICA
+                                                   join e in db.Especialidad on d.IIDESPECIALIDAD equals e.IIDESPECIALIDAD
+                                                   where d.BHABILITADO == 1
+                                                   select new DoctorModel
+                                                   {
+                                                       IdDoctor = d.IIDDOCTOR,
+                                                       NombreCompleto = d.NOMBRE + " " + d.APMATERNO,
+                                                       NombreClinica = c.NOMBRE,
+                                                       NombreEspecialidad = e.NOMBRE,
+                                                       Email = d.EMAIL,
+                                                       FechaContrato = (DateTime)d.FECHACONTRATO
+                                                   }).ToList();
+
+            return listDoctor;
+        }
+    }
+}
---
+++ AppWebApi/AppWebApi/Models/DoctorModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppWebApi.Models
+{
+    public class DoctorModel
+    {
+        public int IdDoctor { get; set; }
+        public string NombreCompleto { get; set; }
+        public string NombreClinica { get; set; }
+        public string NombreEspecialidad { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaContrato{ get; set; }
+    }
+}
